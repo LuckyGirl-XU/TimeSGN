43dfb4a03f3581a3e1f06792d958d5c8c1bc6483
LuckyGirl-XU <121740199+LuckyGirl-XU@users.noreply.github.com>
2023-11-30 14:15:35 +1100

    Add files via upload

---
+++ EdgePredictor.py
@@ -0,0 +1,18 @@
+import torch
+class EdgePredictor(torch.nn.Module):
+
+    def __init__(self, dim_in):
+        super(EdgePredictor, self).__init__()
+        self.dim_in = dim_in
+        self.src_fc = torch.nn.Linear(dim_in, dim_in)
+        self.dst_fc = torch.nn.Linear(dim_in, dim_in)
+        self.out_fc = torch.nn.Linear(dim_in, 1)
+
+    def forward(self, h, neg_samples=1):
+        num_edge = h.shape[0] // (neg_samples + 2)
+        h_src = self.src_fc(h[:num_edge])
+        h_pos_dst = self.dst_fc(h[num_edge:2 * num_edge])
+        h_neg_dst = self.dst_fc(h[2 * num_edge:])
+        h_pos_edge = torch.nn.functional.relu(h_src + h_pos_dst)
+        h_neg_edge = torch.nn.functional.relu(h_src.tile(neg_samples, 1) + h_neg_dst)
+        return self.out_fc(h_pos_edge), self.out_fc(h_neg_edge)
